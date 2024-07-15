f1f296b5eda0e8cbe7d0ba6fe9b865b23b25859b
jonesr6 <37814693+jonesr6@users.noreply.github.com>
2024-07-15 15:14:03 -0400

    Update Average_by_region.m
    Signed-off-by: jonesr6 <37814693+jonesr6@users.noreply.github.com>

--- Average_by_region.m
+++ Average_by_region.m
@@ -9,31 +9,25 @@ function powdop2 = Average_by_region(PowDoppler,st,AtlasCrop)
 
 nregions=length(st.id);
 [X,Y,Z,T] = size(PowDoppler);
-
 Regions = [];de=[];l=[];e=[];
 powdop2=zeros(nregions,size(PowDoppler,4));
 for ii=1:nregions
-structure_id = st.id(ii);
-structure_name = st.safe_name(ii);
-[r,c,d] = ind2sub(size(AtlasCrop),find(AtlasCrop==structure_id)); % [row,col,depth]
-idc = find(r>0 & r<=X & c>0 & c<=Y & d>0 & d<=Z);
-r2=r(idc);c2=c(idc);d2=d(idc);
-Regions =  [Regions; {structure_name}, {r2},{c2},{d2}];
-de = [de;r2];l=[l;c2];e=[e;d2];
-
-idx=zeros(length(idc),1);
- for kk=1:T
-     oo=1;
-for jj=1:length(idc)
-    %for kk=1:size(PowDoppler,4)
-        powdop2(ii,kk)=powdop2(ii,kk)+(PowDoppler(r2(jj),c2(jj),d2(jj),kk));
-    oo=oo+1;
-        %end
-end 
-powdop2(ii,kk)=powdop2(ii,kk)/oo;
-end
-%powdop2(ii,:)=mean(mean(mean(PowDoppler(idc))));
-ii
+  structure_id = st.id(ii);
+  structure_name = st.safe_name(ii);
+  [r,c,d] = ind2sub(size(AtlasCrop),find(AtlasCrop==structure_id)); % [row,col,depth]
+  idc = find(r>0 & r<=X & c>0 & c<=Y & d>0 & d<=Z);
+  r2=r(idc);c2=c(idc);d2=d(idc);
+  Regions =  [Regions; {structure_name}, {r2},{c2},{d2}];
+  de = [de;r2];l=[l;c2];e=[e;d2];
+  idx=zeros(length(idc),1);
+  for kk=1:T
+    oo=1;
+    for jj=1:length(idc)
+      powdop2(ii,kk)=powdop2(ii,kk)+(PowDoppler(r2(jj),c2(jj),d2(jj),kk));
+      oo=oo+1;
+    end 
+  powdop2(ii,kk)=powdop2(ii,kk)/oo;
+  end
 end
 
 end
