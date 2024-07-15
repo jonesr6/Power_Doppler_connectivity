64a0ec37e4b8bfdfdc4aef274affcad3d24602da
jonesr6 <37814693+jonesr6@users.noreply.github.com>
2024-07-15 14:32:10 -0400

    Add files via upload
    Signed-off-by: jonesr6 <37814693+jonesr6@users.noreply.github.com>

---
+++ Average_by_region.m
@@ -0,0 +1,43 @@
+function powdop2 = Average_by_region(PowDoppler,st,AtlasCrop)
+% Average_by_region: Spatially average power doppler data by brain region
+% PowDoppler: Power doppler data (depth,lateral,elevational,time)
+% st.safe_name: structure name
+% st.id: structure id
+% AtlasCrop: Atlas registered to brain power doppler volume, labeled with
+%            structure id's from st.id
+% powdop2: Power doppler data, averaged by brain region (number of brain regions, time)
+
+nregions=length(st.id);
+[X,Y,Z,T] = size(PowDoppler);
+
+Regions = [];de=[];l=[];e=[];
+powdop2=zeros(nregions,size(PowDoppler,4));
+for ii=1:nregions
+structure_id = st.id(ii);
+structure_name = st.safe_name(ii);
+[r,c,d] = ind2sub(size(AtlasCrop),find(AtlasCrop==structure_id)); % [row,col,depth]
+%r = r + dx; % Compensate for moving the volume
+%c = c + dy; % Compensate for moving the volume
+%d = d + dz; % Compensate for moving the volume
+idc = find(r>0 & r<=X & c>0 & c<=Y & d>0 & d<=Z);
+r2=r(idc);c2=c(idc);d2=d(idc);
+Regions =  [Regions; {structure_name}, {r2},{c2},{d2}];
+de = [de;r2];l=[l;c2];e=[e;d2];
+
+idx=zeros(length(idc),1);
+ for kk=1:T
+     oo=1;
+for jj=1:length(idc)
+    %for kk=1:size(PowDoppler,4)
+        powdop2(ii,kk)=powdop2(ii,kk)+(PowDoppler(r2(jj),c2(jj),d2(jj),kk));
+    oo=oo+1;
+        %end
+end 
+powdop2(ii,kk)=powdop2(ii,kk)/oo;
+end
+%powdop2(ii,:)=mean(mean(mean(PowDoppler(idc))));
+ii
+end
+
+end
+
---
+++ calculate_connectivity_pearson.m
@@ -0,0 +1,27 @@
+function ConnMat = calculate_connectivity_pearson(PD_reorg)
+%calculate_connectivity_pearson: This function calculates the connectivity
+%between voxels in time, using Pearson correlation between voxels
+%PD_reorg: power doppler matrix collapsed into voxels x time
+%ConnMat: connectivity between each pixel through time, only populating top
+%triangle of matrix
+
+ConnMat = zeros(size(PD_reorg,1))+ 0.0;
+first_flag = 1;
+for k1=1:size(PD_reorg,1)
+%disp(['Progress : ' num2str(round(100*k1/size(tmp_pDoppler_reorg,1))) '%'])
+for k2=k1:size(PD_reorg,1)
+if first_flag
+c = corrcoef(PD_reorg(k1,:),PD_reorg(k2,:));
+CLag0 = c(1);
+first_flag = 0;
+else
+c = corrcoef(PD_reorg(k1,:),PD_reorg(k2,:));
+CLag0 = c(2);
+end
+ConnMat(k1,k2) = abs(CLag0);
+end
+clc
+k1
+end
+
+end
